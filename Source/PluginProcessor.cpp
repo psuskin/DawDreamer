eb594eac529209018f0ca7f0702d793cd6f9c396
Philip Suskin <philip.suskin@classix.de>
2023-04-03 22:20:46 +0200

    Fixed loadState for server by removing temporary window

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -400,7 +400,7 @@ void PluginProcessor::loadStateInformation(std::string filepath) {
   // todo: this is a little hacky. We create a window because this forces the
   // loaded state to take effect in certain plugins. This allows us to call
   // load_state and not bother calling open_editor().
-  StandalonePluginWindow tmp_window(*this, *myPlugin);
+  //StandalonePluginWindow tmp_window(*this, *myPlugin);
   juce::MessageManager::getInstance()->runDispatchLoopUntil(10);
 }
 
